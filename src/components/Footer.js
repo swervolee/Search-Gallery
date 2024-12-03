4cef993ee95d3785082382d294838410c46778bc
monalisa <williamkubai101@gmail.com>
2024-12-03 20:54:00 +0300

    Fixing styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                        © 2023 <a href="https://search-gallery-sandy.vercel.app" className="hover:underline">Search Gallery™</a>. All Rights Reserved.
+                        © 2024 <a href="https://search-gallery-sandy.vercel.app" className="hover:underline">Search Gallery™</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" aria-label="Facebook page">
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,6 @@ import Container from "./Container";
 export default function Search() {
   const { searchTerm } = useParams();
 
-  console.log(searchTerm);
-
   return (
     <div>
       <Container searchTerm={searchTerm} />
