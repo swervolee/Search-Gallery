51e955bafd083debd45217af02aab4116f629e9c
monalisa <williamkubai101@gmail.com>
2024-12-04 23:25:58 +0300

    Styling the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ export default function Footer() {
             className="bg-spotifyBlack rounded pt-5 fixed bottom-0 left-0 w-full flex flex-wrap justify-center items-center"
             style={{ zIndex: 1000 }}
         >
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex items-center justify-center">
+            <div className="mx-auto w-full max-w-screen-xl lg:py-8 flex items-center justify-center">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-center text-gray-500 sm:text-center dark:text-gray-400">
                         © 2024 <a href="https://search-gallery-sandy.vercel.app" className="hover:underline">Search Gallery™</a>
                     </span>
-                    <div className="flex mt-4 sm:justify-center sm:mt-0">
+                    <div className="flex mt-1 sm:justify-center sm:mt-0">
                         <a
                             href="#"
                             className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
